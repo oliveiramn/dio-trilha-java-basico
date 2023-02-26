d458f9de753b09f0ee2312220270fb4aff10daf3
oliveiramn <mnoliveira1995@gmail.com>
2023-02-26 09:55:27 -0300

    Atualizado até operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main (String [] args) throws Exception {
+
+        double salarioMinimo =2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        int numero = 5;
+
+        numero = 10;
+        final double VALOR_DE_PI = 3.14;
+
+
+        System.out.print(VALOR_DE_PI);
+
+
+        
+       
+        
     }
+
 }
+
